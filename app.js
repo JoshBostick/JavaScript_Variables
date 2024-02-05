acf5a0bdd9b3c25b9249e8c72be22b07f9f5b452
Josh Bostick <jbostick99@gmail.com>
2024-02-05 12:59:48 -0600

    finished

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ console.log(
 
 // Exercise 1
 
+const firstName = "Josh";
+let lastName;
+var age;
+
 // Exercise 2
 
+lastName = "Bostick";
+age = 43;
+
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Excersie 4
+
+let price = 19.99;
+let IsOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let PageCount = 722;
+let bookmark = 456;
+let hasRead = true;
